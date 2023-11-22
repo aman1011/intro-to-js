c198ca09c34e8166fdf02ca7f0109f7e3b0b014e
Aman Gaur <aman.gaur1011@gmail.com>
2023-11-22 16:11:24 -0400

    adding other files

---
+++ main.js
@@ -0,0 +1,8 @@
+document.write("<section>");
+document.write("<h3>Education</h3>");
+document.write("</section>");
+
+
+document.write("<section>");
+document.write("<h3>Hobbies / Interests</h3>");
+document.write("</section>");
